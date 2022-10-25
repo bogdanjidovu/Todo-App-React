5f761bbab2b1c18e32364f3e18c28474516ee6f2
Bogdan Jidovu <bogdanjidovu98@gmail.com>
2022-10-25 21:19:50 +0300

    Updates

---
+++ precache-manifest.31c9af52e96d23a2b17d3c64ed80631f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1db74bd622449583c9d9",
+    "url": "/todo-app-react/static/css/main.599337f3.chunk.css"
+  },
+  {
+    "revision": "1db74bd622449583c9d9",
+    "url": "/todo-app-react/static/js/main.05e95107.chunk.js"
+  },
+  {
+    "revision": "012f357a5be08e394745",
+    "url": "/todo-app-react/static/js/runtime~main.e834fa13.js"
+  },
+  {
+    "revision": "d0b756b89f9e34634bf9",
+    "url": "/todo-app-react/static/js/2.e92b5004.chunk.js"
+  },
+  {
+    "revision": "39de776815a99137d835",
+    "url": "/todo-app-react/static/js/3.ac1c91d6.chunk.js"
+  },
+  {
+    "revision": "ffbbd46a663483436c8ad88710eba385",
+    "url": "/todo-app-react/index.html"
+  }
+];
